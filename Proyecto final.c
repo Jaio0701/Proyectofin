ea421a210803868d88f19c6f5522f440839f7d1e
Jaio0701 <74441415+Jaio0701@users.noreply.github.com>
2020-11-20 12:55:57 -0600

    Update Proyecto final.c

--- Proyecto final.c
+++ Proyecto final.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 void iniciarTablero();
 void mostrarTablero(int band); 
 void leerCoordenadas(FILE *f);
+void archivo(char nombre[],int tam, int edad, int ganada, int barcos);
 
 int tablero[10][10];
 
@@ -10,16 +14,37 @@ int main()
 {
 	FILE *f;
 	
+	char nombre[4];
+	int edad;
+	printf("Escribe tu nombre(4 letras) : ");
+	scanf("%[^\n]", nombre);
+	fflush(stdin);
+	while((strlen(nombre))!=4 ){
+		fflush(stdin);
+		printf("Escribe tu nombre(4 letras) : ");
+		scanf("%[^\n]", nombre);
+		fflush(stdin);
+	}
+	printf("cual es tu edad : ");
+	scanf("%d",&edad);
+	if(edad <= 17){
+		printf("LO SIENTO NO PUEDES JUGAR, TIENES QUE SER MAYOR DE EDAD!!!!\n");
+		system("pause");
+		return 0;
+	}
+	
 	iniciarTablero();
 	mostrarTablero(0);
 	leerCoordenadas(f);
 	printf("\n");
 	mostrarTablero(1);
-		
+	
 	printf("\n");		
 	system("pause");
 	return 1;
 }
+
+
 void iniciarTablero()
 {
 	int i,j;
@@ -32,15 +57,17 @@ void iniciarTablero()
 		}
 	}
 }
+
+
 void mostrarTablero(int band	)
 {
 	int i,j;
 	
 	printf("   ");										
 	for(i = 0; i < 10; i++)
-	printf("%d   ",i);
+		printf("%d   ",i);
 	printf("\n");
-
+	
 	for(i = 0; i < 10; i++)
 	{
 		printf("%d  ", i);
@@ -50,17 +77,18 @@ void mostrarTablero(int band	)
 			if(band)
 			{
 				if(tablero[i][j] == 1)
-				
-				printf("%c   ", 254);
+					
+					printf("%c   ", 254);
 				else
-				printf("%c   ", 120);
+					printf("%c   ", 120);
 			}
-				else printf("%c   ",120);	
+			else printf("%c   ",120);	
 		}
 		
-	printf("\n");
+		printf("\n");
 	}
 }   
+
 
 void leerCoordenadas(FILE *f)
 {
@@ -83,11 +111,38 @@ void leerCoordenadas(FILE *f)
 		for(i = 0; i < 10; i++)
 		{
 			fscanf(f,"%d%c%d", &line, &car, &col);
-		
-				for(j = 0; j < 3; j++)
-				{
-					tablero[line ][col+j] = 1;
-				}
+			
+			for(j = 0; j < 3; j++)
+			{
+				tablero[line ][col+j] = 1;
+			}
 		}		
 	}
 } 
+
+
+void archivo(char nombre[], int tam, int edad, int ganada, int barcos){
+	/*la funcion recibe 4 parametros
+	char nombre[]: es la cadena del nombre
+	int edad: la edad
+	int ganada: recibe un 1 o un 0, si el jugador gana guardara 1
+	y si pierde guardara un 0
+	int barcos: recibe el numero de barcos que logo hundir si perdio*/
+	
+	FILE * flujo;
+	flujo = fopen("informacion.txt", "a");
+	if(flujo == NULL){
+		perror("Error al crear el archivo\n");
+	} else {
+		if(ganada==1){
+			fprintf(flujo, "%s     %d     ganador",nombre, edad);
+			fputc('\n', flujo);
+		} else{
+			fprintf(flujo, "%s     %d     perdedor  %d    barcos hundidos",nombre, edad, barcos);
+			fputc('\n', flujo);
+		}
+		fflush(flujo);
+		fclose(flujo);
+		printf("DATOS GUARDADOS EXITOSAMENTE!!!!!\n\n");
+	}
+}
